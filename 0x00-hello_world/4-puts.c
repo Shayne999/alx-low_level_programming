3e1278d511e4a77fb722f727bb43285989496788
Shayne999 <shaynejuniorn@gmail.com>
2023-07-13 12:26:24 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/*
+ * main - A programme/function that uses puts to print out a sentencs
+ * return 0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
