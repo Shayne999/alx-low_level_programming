e3e9b76946cb900e3721eaa224c1b2e6405b715e
Shayne999 <shaynejuniorn@gmail.com>
2023-07-14 15:15:02 -0700

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Functions uses putchat to print numbers in a given range.
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <+ 9; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+	return (0);
+}
