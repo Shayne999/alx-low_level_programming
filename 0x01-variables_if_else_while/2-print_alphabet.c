b321432975cc30ff43651ed7e6398140986cb569
Shayne999 <shaynejuniorn@gmail.com>
2023-07-14 13:43:31 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function prints out the alphabet
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
