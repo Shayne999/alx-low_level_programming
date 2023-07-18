6bcfec85a6b1cafff4feb6ca1ede2df3ff17a32f
Shayne999 <shaynejuniorn@gmail.com>
2023-07-18 05:34:34 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - entry point
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int n = 0;
+
+	while (n < 9)
+	{
+		for (i = 'a'; i < 'z'; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	n++;
+	}
+
+}
