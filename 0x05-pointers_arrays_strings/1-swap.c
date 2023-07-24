ba19901919119bf9de487e6d99af0f2f8b6f8f94
Shayne999 <shaynejuniorn@gmail.com>
2023-07-24 11:01:59 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - function entry point
+ * @a: parameter to be swapped.
+ * @b: second paremeter to be swapped
+ * @x: temporary variable
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	*a = *b;
+	*b = x;
+}
