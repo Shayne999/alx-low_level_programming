6d9e4c30bba40fa0d9ceb8b82234e0faf257bfc8
Shayne999 <shaynejuniorn@gmail.com>
2023-07-31 05:40:06 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function searches for a string for any set of bytes
+ * @s: source string pointer
+ * @accept: acceptable characters string pointer
+ * Return:  null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (s[i] == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
+
