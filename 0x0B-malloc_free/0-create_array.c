e1da91ddbf171c5061b9ac6c5b7eaea5bed5589e
Shayne999 <shaynejuniorn@gmail.com>
2023-08-07 03:48:54 -0700

    Create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array with the specified parameters
+ * @size: unsigned int
+ * @c: character
+ * Return: str
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(size * sizeof(char));
+
+	if (str == '\0')
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains files with the .c extension as well as compiled files. The files contains solutions to the Alx 0x0B. C - malloc, free task.
