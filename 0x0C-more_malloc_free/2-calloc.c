87774aad0803a2e5d126d0b15087309fffba09aa
Shayne999 <shaynejuniorn@gmail.com>
2023-08-10 03:59:55 -0700

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int  *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = (int *)malloc(nmemb * size);
+	ptr = (unsigned int *)malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = 0;
+	}
 	return (ptr);
 }
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of intergers
+ * @min:
+ * @max:
+ * Return:
+ */
+
+int *array_range(int min, int max)
+{
+
+}
