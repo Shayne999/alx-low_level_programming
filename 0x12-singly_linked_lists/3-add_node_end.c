4f5625130a8ce93777e79919ba1a9038314efbff
Shayne999 <shaynejuniorn@gmail.com>
2023-08-24 09:54:38 -0700

    Add end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,6 +15,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *ptr = *head;
 	list_t *new;
 
+	while (str[len])
+		len++;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
