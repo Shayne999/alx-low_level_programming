ff2c0098c038ef074cba1a1000a77a4123a8b7be
Shayne999 <shaynejuniorn@gmail.com>
2023-08-28 12:27:16 -0700

    First commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: list
+ * Return: count
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	const listint_t *ptr = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (ptr != NULL)
+	{
+		printf("%d", ptr->n);
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory ontains a header file, files with the .c extension as well as compiled files. The contents of these files are solutions to the Alx 0x13-more_singly_linked_lists task.
