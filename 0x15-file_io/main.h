cad6e52a58158e72680dcad28e8d1b29a9f447fe
Shayne999 <shaynejuniorn@gmail.com>
2023-10-30 04:19:19 -0700

    Second commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -24,17 +24,4 @@ void error_100(int file_zero, char *buff);
 
 int main(int argc, char **argv);
 
-void check_elf(unsigned char *e_id);
-void magic_print(unsigned char *e_id);
-void print_class(unsigned char *e_id);
-void print_data(unsigned char *e_id);
-void version(unsigned char *e_id);
-void print_osabi(unsigned char *e_id);
-void print_abi(unsigned char *e_id);
-void e_type(unsigned int etype, unsigned char *e_id);
-void e_entry(unsigned long int entry, unsigned char *e_id);
-void e_entry(unsigned long int entry, unsigned char *e_id);
-void close_elf(int elf);
-int main(int argc, char *argv[]);
-
 #endif
