a8aa891c6af5d08a87da39f84d2f570c92643beb
Shayne999 <shaynejuniorn@gmail.com>
2023-12-07 04:13:57 -0800

    Q0

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef "LISTS_H"
 #define "LISTS_H"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
